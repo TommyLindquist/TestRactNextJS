4fe498e04a58ce7fcad58a196d5e3e5eebece2b8
Tommy <Tommy@DESKTOP-UJVT1RG>
2025-08-20 17:56:05 +0200

    added slug

---
+++ app/lib/data/slugArray.ts
@@ -0,0 +1,24 @@
+export const pages = [
+  {
+    slug: "nameofanything",
+    label: "nameofanything",
+    title: "Welcome to the nameofanything!",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At saepe, illo itaque eos voluptatem dolorum recusandae cumque quibusdam optio consequatur necessitatibus excepturi adipisci nesciunt inventore, hic aliquid, ab velit voluptates.",
+  },
+  {
+    slug: "toys",
+    label: "Toys",
+    title: "See our toys",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. At saepe, illo itaque eos voluptatem dolorum recusandae cumque quibusdam optio consequatur necessitatibus excepturi adipisci nesciunt inventore, hic aliquid, ab velit voluptates.",
+  },
+  {
+    slug: "colors",
+    label: "Colors",
+    title: "Find your inner color",
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt optio libero excepturi. Labore eveniet vitae possimus. Et, a? Tempore fugiat molestiae quod. Eos alias, fugiat velit officia accusamus odio porro.
+  Nisi nemo reiciendis eum dignissimos numquam exercitationem sapiente laudantium odit aperiam. Amet facere optio sapiente veniam. Corporis quia labore velit nulla? Harum sapiente veniam velit nihil consequuntur excepturi, rerum adipisci.
+  Incidunt, reprehenderit nihil quisquam nisi at, delectus, dicta odio atque autem enim corrupti suscipit porro sunt? Quis nulla consectetur eos necessitatibus at temporibus, fuga illo unde! Laudantium facilis nostrum atque?`,
+  },
+];
